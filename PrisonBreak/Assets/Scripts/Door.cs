2cb01b63b262f198848e33e37b684a8da579901c
Arvid98 <111972825+Arvid98@users.noreply.github.com>
2023-06-03 10:31:46 +0200

    door

---
+++ PrisonBreak/Assets/Scripts/Door.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] bool open;
+    [SerializeField] private GameObject visualGameObject;
+    [SerializeField]
+    private void Awake()
+    {
+        GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (open)
+        {
+            Hide();
+            GetComponent<Collider2D>().enabled = false;
+        }
+        else if(!open)
+        {
+            GetComponent<Collider2D>().enabled = true;
+            Show();
+        }
+    }
+
+    private void Show()
+    {
+        visualGameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        visualGameObject.SetActive(false);
+    }
+}
